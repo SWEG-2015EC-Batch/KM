e0b0e53f145e9305461fb4fb66e9f34a39cfdfa6
KalMesKeb <149013842+KalMesKeb@users.noreply.github.com>
2023-12-18 09:40:57 -0800

    Update Exercise 3.cpp

--- ETS0742_15_Kaleb_Mesfin/Activity 3.1/Exercise 3.cpp
+++ ETS0742_15_Kaleb_Mesfin/Activity 3.1/Exercise 3.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <ctype.h>
 
+using namespace std;
+
+int main() {
+  
+    char a;
+  
+    cout << "Enter a letter: ";
+    cin >> a;
+
+	if (islower(a)) { 
+  
+        cout << "The entered character is a lower case letter." << endl; 
+  
+	}
+		
+	else if (isupper(a)) {  
+
+		cout << "The entered character is an upper case letter." << endl; 
+	}
+	
+	else if (isdigit(a)) { 
+    
+		cout << "The entered character is a digit." << endl;
+    
+	}
+	
+    return 0;
+}
