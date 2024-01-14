7ebb4cac800b3a09c32b4fa16f0552599e070c54
KalMesKeb <149013842+KalMesKeb@users.noreply.github.com>
2024-01-14 10:14:49 -0800

    Update Exercise 7.cpp

--- ETS0742_15_Kaleb_Mesfin/Activity 4.2/1D-Arrays/Exercise 7.cpp
+++ ETS0742_15_Kaleb_Mesfin/Activity 4.2/1D-Arrays/Exercise 7.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
 
+using namespace std;
+
+int main(){
+  
+  int N;
+  
+  cout << "Enter the amount of numbers you want to be entered: ";
+  cin >> N;
+  
+  int Numbers[N];
+  
+  cout<<"Enter numbers: ";
+  
+  for (int i = 0; i < n; i++){
+    
+    int l = i + 1;
+    cout << "Enter number #" << l << ": ";
+    cin >> Numbers[i];
+  
+  }
+
+  cout << "The numbers are as follows: ";
+  
+  for (int i = n - 1; i >= 0; i--){
+    
+    if (Numbers[i] % 2 == 0){
+      
+      cout << Numbers[i] << " ";
+    
+    }
+    
+  }
+  
+  return 0;
+
+}
