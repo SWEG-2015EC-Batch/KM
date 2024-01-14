ad7018487bf4cb53f876a9a78c4cc34b3233bd79
KalMesKeb <149013842+KalMesKeb@users.noreply.github.com>
2024-01-14 09:58:13 -0800

    Update Exercise 2.cpp

--- ETS0742_15_Kaleb_Mesfin/Activity 4.2/1D-Arrays/Exercise 2.cpp
+++ ETS0742_15_Kaleb_Mesfin/Activity 4.2/1D-Arrays/Exercise 2.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
 
+using namespace std;
+
+int main(){
+  
+  int temp [8], Sum = 0, Mean = 0;
+  
+  cout<<"Enter eight integers: ";
+  
+  for (int i = 0; i < 8; i++){
+    
+    cin >> temp[i];
+    Sum += temp[i];
+  
+  }
+  
+  Mean = Sum / 8;
+  
+  for (int i = 0; i < 8; i++){
+    
+    int l = i + 1;
+    cout << "Number #" << l << " is: " << temp[i] << endl;
+  
+  }
+  
+  cout << "The average of the numbers given is: "<< Mean << endl;
+  
+  return 0;
+
+}
